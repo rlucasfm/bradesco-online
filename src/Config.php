424e3b00a253dca38f862bcfc71a93a30be6911b
rlucasfm <richardlucasfm@gmail.com>
2021-05-25 15:35:45 -0300

    v1.0

--- src/Config.php
+++ src/Config.php
@@ -1,8 +1,7 @@
 <?php 
 
 define('ENDPOINT', 'https://cobranca.bradesconetempresa.b.br/ibpjregistrotitulows/registrotitulohomologacao');  
-define('CERT_PATH', '../resources/2793032.pfx');  
+define('CERT_PATH', '2793032.pfx');  
 define('CERT_PASS', '654321');  
 define('TIMEOUT', 30);  
 define('CACERTPATH', "C:\wamp64\bin\php\php7.4.9\\extras\ssl\cacert.pem");  
-define('TEMP', '../resources/');
