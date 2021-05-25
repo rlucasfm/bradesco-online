7b9c728b0214b0c8a7f86af0bf2ad28a8c1dc8a5
rlucasfm <richardlucasfm@gmail.com>
2021-05-25 14:48:49 -0300

    v1.0

---
+++ src/Api.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BradescoOnline;
+
+include('Config.php');
+
+class Api
+{
+    protected $arr = [];    
+    protected $cert_path = CERT_PATH;
+    protected $cert_pass = CERT_PASS;    
+    protected $certKey;
+    protected $privateKey;
+    protected $signed;
+
+    public function __construct(array $array)
+    {
+        $this->set_arr($array);                       
+        $this->setCertKeys();
+
+        $this->signed = $this->encryptData($this->arr);
+        return $this;        
+    }
+
+    public function get_signed()
+    {
+        return $this->signed;
+    }
+
+    public function set_arr($array)
+    {
+        $this->arr = $array;
+    }
+
+
+    public function setCertKeys()
+    {
+        $cert_file = file_get_contents($this->cert_path);
+        if (!openssl_pkcs12_read($cert_file, $result, $this->cert_pass)) {
+            throw new \Exception('Unable to read certificate file .pfx. Please check the certificate password.');
+        }
+
+        $this->certKey = openssl_x509_read($result['cert']);
+        $this->privateKey = openssl_pkey_get_private($result['pkey'], $this->cert_pass);
+    }
+
+    private function encryptData($data)
+    {
+        $msgFile = TEMP . 'jsonFile';
+        $signedFile = TEMP . 'signedFile';
+        file_put_contents($msgFile, $data);
+
+        openssl_pkcs7_sign(
+            $msgFile, $signedFile, $this->certKey, $this->privateKey, [], PKCS7_BINARY | PKCS7_TEXT
+        );
+
+        $signature = file_get_contents($signedFile);
+        $parts = preg_split("#\n\s*\n#Uis", $signature);
+        $signedMessageBase64 = $parts[1];
+
+        unlink($msgFile);
+        unlink($signedFile);
+
+        return $signedMessageBase64;
+    }
+}
+
---
+++ src/BradescoClient.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BradescoOnline;
+
+require '../vendor/autoload.php';
+require 'Api.php';
+
+use BradescoOnline\Api;
+use GuzzleHttp\Client;
+
+class BradescoClient 
+{    
+    protected $client;
+    protected $enc_data;
+
+    public function __construct($data)
+    {
+        $api = new Api($data);
+        
+        $this->client = new Client([
+            'base_uri' => ENDPOINT,
+            'timeout' => TIMEOUT,
+            'verify' => false
+        ]);
+
+        $this->enc_data = $api->get_signed();               
+    }
+
+    public function send_request()
+    {        
+        try {
+            $response = $this->client->request('POST', '', ['body' => $this->enc_data]);
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+
+        return $response;
+    }
+}
+
