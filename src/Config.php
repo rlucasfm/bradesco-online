34153029cf7c1c3a06ca83b0b3dd62ecbf348c0a
rlucasfm <richardlucasfm@gmail.com>
2021-05-25 16:14:08 -0300

    v1.02

--- src/Config.php
+++ src/Config.php
@@ -1,7 +1,7 @@
 <?php 
 
 define('ENDPOINT', 'https://cobranca.bradesconetempresa.b.br/ibpjregistrotitulows/registrotitulohomologacao');  
-define('CERT_PATH', '2793032.pfx');  
-define('CERT_PASS', '654321');  
+define('CERT_PATH', 'nomedoarquivocertificado.pfx'); // Insira o nome do seu arquivo certificado. Coloque-o dentro da pasta src  
+define('CERT_PASS', '123456'); // A senha do certificado .pfx aqui
 define('TIMEOUT', 30);  
-define('CACERTPATH', "C:\wamp64\bin\php\php7.4.9\\extras\ssl\cacert.pem");  
+define('CACERTPATH', "C:\wamp64\bin\php\php7.4.9\\extras\ssl\cacert.pem"); // Edite para onde estiverem os seus certificados. Não é necessário. 
